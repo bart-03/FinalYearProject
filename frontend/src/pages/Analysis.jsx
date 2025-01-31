07a7af60ca430de7c1bb2be67a16c67ba20b3386
bart-03 <bartekkfraczek@gmail.com>
2025-01-31 21:52:57 +0000

    refractoring, remove  image button and image preview css

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -4,6 +4,7 @@ import Select from "react-select";
 import ReusableSection from "./Report";
 import axios from "axios";
 import { MyContext } from "./MyContext";
+import questions from "./PromptQuestions";
 
 const Analysis = () => {
   const [checked1, setChecked1] = useState(true);
@@ -14,13 +15,35 @@ const Analysis = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [response, setResponse] = useState(null);
+  const [answers, setAnswers] = useState(false);
+  const [image, setImage] = useState(null);
+  const [imageLocal, setImageLocal] = useState(null);
+
   const dropdownRef = useRef(null);
   const checkHandler1 = () => setChecked1(!checked1);
   const checkHandler2 = () => setChecked2(!checked2);
 
   const { navbarValue } = useContext(MyContext);
-
   const [isNavbarOpen, setIsNavbarOpen] = useState(navbarValue);
+
+  let render;
+  if (selectedOptions.length === 0) {
+    render = (
+      <div className="nothing-selected-content">Please select a disease.</div>
+    );
+  }
+
+  let render2;
+  if (image) {
+    render2 = (
+      <button
+        className="remove-img-button"
+        onClick={() => setImageLocal(null) && setImage(null)}
+      >
+        X
+      </button>
+    );
+  }
 
   useEffect(() => {
     setIsNavbarOpen(navbarValue);
@@ -50,141 +73,6 @@ const Analysis = () => {
     setBothButtonPressed(false);
   }, [checked1, checked2]);
 
-  const questions = [
-    { id: 1, text: "What is the patient’s age?", type: "input" },
-    { id: 2, text: "What is the patient’s sex?", type: "input" },
-    {
-      id: 3,
-      text: "What is the patient’s weight? (in kg)",
-      type: "input",
-    },
-    {
-      id: 4,
-      text: "What is the patient’s height? (in cm)",
-      type: "input",
-    },
-    { id: 5, text: "Does the patient have a fever?", type: "checkbox" },
-    { id: 6, text: "Is the patient experiencing a cough?", type: "checkbox" },
-    {
-      id: 7,
-      text: "If productive, what is the color of the sputum?",
-      type: "input",
-    },
-    {
-      id: 8,
-      text: "Is the patient experiencing shortness of breath?",
-      type: "checkbox",
-    },
-    { id: 9, text: "Does the patient have chest pain?", type: "checkbox" },
-    { id: 10, text: "Does the patient have wheezing?", type: "checkbox" },
-    {
-      id: 11,
-      text: "Has the patient experienced loss of smell or taste?",
-      type: "checkbox",
-    },
-    {
-      id: 12,
-      text: "Does the patient have fatigue or weakness?",
-      type: "checkbox",
-    },
-    {
-      id: 13,
-      text: "Is the patient experiencing chills or rigors?",
-      type: "checkbox",
-    },
-    {
-      id: 14,
-      text: "Is the patient experiencing a sore throat?",
-      type: "checkbox",
-    },
-    { id: 15, text: "Does the patient have muscle aches?", type: "checkbox" },
-    {
-      id: 16,
-      text: "Is the patient experiencing nausea, vomiting, or diarrhea?",
-      type: "checkbox",
-    },
-    {
-      id: 17,
-      text: "Is the patient experiencing severe difficulty breathing?",
-      type: "checkbox",
-    },
-    {
-      id: 18,
-      text: "Has the patient fainted or felt dizzy?",
-      type: "checkbox",
-    },
-    {
-      id: 19,
-      text: "Is there bluish discoloration of the lips or nails (cyanosis)?",
-      type: "checkbox",
-    },
-    {
-      id: 20,
-      text: "Does the patient feel chest tightness or heaviness?",
-      type: "checkbox",
-    },
-    {
-      id: 21,
-      text: "Does the patient have a history of lung disease?",
-      type: "input",
-    },
-    {
-      id: 22,
-      text: "Does the patient have a history of heart disease?",
-      type: "input",
-    },
-    {
-      id: 23,
-      text: "Does the patient take any medications regularly? (Please list)",
-      type: "input",
-    },
-    {
-      id: 24,
-      text: "Does the patient have known allergies? (Please List)",
-      type: "input",
-    },
-    {
-      id: 25,
-      text: "Does the patient have a history of smoking?",
-      type: "checkbox",
-    },
-    {
-      id: 26,
-      text: "Has the patient been in contact with someone sick or tested positive for COVID-19?",
-      type: "checkbox",
-    },
-    { id: 27, text: "Has the patient traveled recently?", type: "input" },
-    {
-      id: 28,
-      text: "Is the patient vaccinated for COVID-19, influenza, and pneumonia?",
-      type: "input",
-    },
-    {
-      id: 29,
-      text: "Does the patient have occupational or environmental exposure to irritants or pollutants?",
-      type: "checkbox",
-    },
-    {
-      id: 30,
-      text: "Does the patient drink alcohol or use recreational drugs?",
-      type: "input",
-    },
-    {
-      id: 31,
-      text: "When did the symptoms start? (dd/mm/yyyy)",
-      type: "input",
-    },
-    {
-      id: 32,
-      text: "Are the symptoms improving, worsening, or unchanged over time? (type 'improving' or 'worseneing'",
-      type: "input",
-    },
-  ];
-
-  const [answers, setAnswers] = useState(false);
-  const [image, setImage] = useState(null);
-  const [imageLocal, setImageLocal] = useState(null);
-
   const handleChange = (id, value) => {
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
@@ -196,20 +84,6 @@ const Analysis = () => {
     setImage(e.target.files[0]);
     setImageLocal(URL.createObjectURL(e.target.files[0]));
   };
-
-  console.log(image);
-  console.log(imageLocal);
-
-  // const handleAnalysis = () => {
-  //   axios
-  //     .post("http://localhost:8080/PnuemoniaPredict", { image })
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     })
-  //     .catch((err) => {
-  //       console.error("Error signing in:", err);
-  //     });
-  // };
 
   if (selectedOptions.length > 0) {
     console.log(selectedOptions[0].value);
@@ -235,11 +109,6 @@ const Analysis = () => {
         });
     }
   };
-
-  let render;
-  if (selectedOptions.length === 0) {
-    render = <div className="nothing-selected">Plaese select a disease.</div>;
-  }
 
   const test = () => {
     setIaButtonPressed(true);
@@ -295,19 +164,20 @@ const Analysis = () => {
                   onMenuOpen={() => setIsDropdownOpen(true)}
                 />
               </div>
-              <div></div>
             </div>
 
             <div className="content">
               {render}
               {imageLocal && (
                 <>
-                  <img
-                    src={imageLocal}
-                    alt="Uploaded Preview"
-                    style={{ maxWidth: "100px" }}
-                  />
-                  <label>Selected Image </label>
+                  <div className="image-preview-container">
+                    <img
+                      className="image-preview"
+                      src={imageLocal}
+                      alt="Uploaded Preview"
+                    />
+                    <label style={{ fontSize: "15px" }}>Selected Image </label>
+                  </div>
                 </>
               )}
 
@@ -316,6 +186,7 @@ const Analysis = () => {
                 className="image-analysis-button1"
                 onChange={handleUpload}
               />
+              {render2}
             </div>
 
             <button
